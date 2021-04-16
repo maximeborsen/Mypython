8012fba178cca5fbe6c504cb791c84c4880b3fd2
Maxime Borsen <62463086+pholper@users.noreply.github.com>
2021-04-16 15:01:03 +0200

    Ajout d'une fonction DisplayFirstname

--- ex1git.py
+++ ex1git.py
@@ -1,6 +1,10 @@
 Nom = input("Quel est votre Nom?")
 Prénom = input("Quel est votre Prénoms")
 def DisplayName():
-    print("Bonjour "+Prénom+" "+Nom)
+    print("Bonjour "+Nom)
+
+def DisplayFirstname():
+    print("Votre prénom est "+Prénom)
 
 DisplayName()
+DisplayFirstname()

a8a5d145a6d0bfdf2bb5c26f2ffa070c3ed625a8
Maxime Borsen <62463086+pholper@users.noreply.github.com>
2021-04-16 15:10:35 +0200

    Rajout de la fonction Displayfullname

--- ex1git.py
+++ ex1git.py
@@ -1,10 +1,6 @@
 Nom = input("Quel est votre Nom?")
 Prénom = input("Quel est votre Prénoms")
-def DisplayName():
-    print("Bonjour "+Nom)
+def Displayfullname():
+    print("Bonjour "+Prénom+" "+Nom".")
 
-def DisplayFirstname():
-    print("Votre prénom est "+Prénom)
-
-DisplayName()
-DisplayFirstname()
+Displayfullname()
